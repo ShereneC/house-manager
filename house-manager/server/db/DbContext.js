060332a5014a6872506c752cf816e6e459c81310
ShereneC <spyhopper@cableone.net>
2021-12-06 11:57:13 -0700

    db fix

--- house-manager/server/db/DbContext.js
+++ house-manager/server/db/DbContext.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
-import { SystemSchema } from '../models/System'
+import { System } from '../models/System'
 
 class DbContext {
-  Systems = mongoose.model('System', SystemSchema);
+  Systems = mongoose.model('System', System);
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
