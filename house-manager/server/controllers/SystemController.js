237b76a116a53ee74153af97739b7b81d0596034
ShereneC <spyhopper@cableone.net>
2021-12-05 21:45:11 -0700

    system controller

---
+++ house-manager/server/controllers/SystemController.js
@@ -0,0 +1,21 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import BaseController from '../utils/BaseController'
+
+export class SystemController extends BaseController {
+  constructor() {
+    super('systems')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('', this.getSystems)
+  }
+
+  async getSystems(req, res, next) {
+    try {
+      const systems = await systemsService.getSystems()
+      console.log(systems)
+      res.send(systems)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
