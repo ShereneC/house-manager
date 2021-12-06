1d57ed3a8b386ec9e544f5264aa4555e5714e0c8
ShereneC <spyhopper@cableone.net>
2021-12-06 11:42:55 -0700

    SysById service

--- house-manager/server/services/SystemsService.js
+++ house-manager/server/services/SystemsService.js
@@ -1,9 +1,18 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
 class SystemsService {
   async getSystems(query = {}) {
     const systems = await dbContext.Systems.find(query)
     return systems
+  }
+
+  async getSystemById(id) {
+    const system = await dbContext.Systems.findById(id).populate('creator')
+    if (!system) {
+      throw new BadRequest('Invalid Id')
+    }
+    return system
   }
 }
 export const systemsService = new SystemsService()
