dbc032b0c26f6a66cad122006085c861d696cb04
ShereneC <spyhopper@cableone.net>
2021-12-06 11:26:39 -0700

    updated model schema

--- house-manager/server/models/System.js
+++ house-manager/server/models/System.js
@@ -1,10 +1,22 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-export const SystemSchema = new Schema(
+export const System = new Schema(
   {
-    name: [{ type: String, required: true }]
+    name: { type: String, required: true },
+    description: { type: String },
+    needsService: { type: Boolean, default: false },
+    lastServiced: { type: String, default: 'Never Serviced' },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+    // images: How do I let them upload images???
 
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
+
+System.virtual('creator', {
+  localField: 'creatorId',
+  ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
