e13dff30267c680255976cf363c442427704bcc5
ShereneC <spyhopper@cableone.net>
2021-12-06 11:30:57 -0700

    controller getbyid

--- house-manager/server/controllers/SystemsController.js
+++ house-manager/server/controllers/SystemsController.js
@@ -7,14 +7,25 @@ export class SystemsController extends BaseController {
     super('api/systems')
     this.router
       .get('', this.getSystems)
+      .get('/:id', this.getSystemById)
       .use(Auth0Provider.getAuthorizedUserInfo)
   }
 
   async getSystems(req, res, next) {
     try {
       const systems = await systemsService.getSystems()
-      console.log(systems)
+      console.log(res)
       res.send(systems)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getSystemById(req, res, next) {
+    try {
+      const system = await systemsService.getSystemById(req.params.id)
+      console.log(res)
+      res.send(system)
     } catch (error) {
       next(error)
     }
