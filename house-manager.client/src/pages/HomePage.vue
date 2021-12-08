817c35287ead01c3d5c95a354e4ec70bead23609
ShereneC <spyhopper@cableone.net>
2021-12-08 11:47:02 -0700

    working on getting systems to page

--- house-manager.client/src/pages/HomePage.vue
+++ house-manager.client/src/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-
+    <h1>Systems</h1>
+    <h6></h6>
+    <h1>Appliances</h1>
   </div>
 </template>
 
