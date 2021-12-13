7d5378adfe88d815b1d36ce173ea78de09fa3468
ShereneC <spyhopper@cableone.net>
2021-12-12 21:42:33 -0700

    appl vue file & appstate

---
+++ house-manager.client/src/components/Appliance.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="col-3 d-flex flex-column rounded m-2 shadow selectable bg-secondary">
+    <h2 class="text-center">{{ appliance.name }}</h2>
+    <img :src="appliance.mainImg" alt="main image" class="rounded main-pic" />
+    <h4>{{ appliance.description }}</h4>
+    <h4 class="service">Needs Service!</h4>
+    <!-- Need a v-if here, but v-if, v-bind, v-for, etc. are not coming up as options when I type v- -->
+
+    <h6>{{ appliance.lastServiced }}</h6>
+    <div class="d-flex justify-content-center">
+      <div class="m-2">
+        <p class="mr-0">{{ appliance.creator.name }}</p>
+      </div>
+      <div class="div">
+        <img :src="appliance.creator.picture" alt="profile image" class="profile-pic ml-2" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    props: {
+      appliance: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .main-pic {
+    width: 100%;
+    height: 12rem;
+    object-fit: cover;
+  }
+
+  .profile-pic {
+    height: 3rem;
+    width: 3rem;
+    border-radius: 50%
+  }
+
+  .service {
+    color: red;
+  }
+</style>
