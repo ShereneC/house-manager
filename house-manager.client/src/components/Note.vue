029a58cda61fd6ee68aadc53d8c71c6c363bd115
ShereneC <spyhopper@cableone.net>
2021-12-23 23:28:56 -0700

    Working on notes

---
+++ house-manager.client/src/components/Note.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'component',
+    setup() {
+      return {}
+    },
+    components: {}
+  }
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
