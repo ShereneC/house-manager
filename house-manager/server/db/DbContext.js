eff0b797a21e5e4dfad209ad9260099a3c3f3317
ShereneC <spyhopper@cableone.net>
2021-12-23 23:33:49 -0700

    db

--- house-manager/server/db/DbContext.js
+++ house-manager/server/db/DbContext.js
@@ -3,10 +3,12 @@ import { AccountSchema, ProfileSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { System } from '../models/System'
 import { Appliance } from '../models/Appliance'
+import { Note } from '../models/Note'
 
 class DbContext {
+  Notes = mongoose.model('Note', Note)
   Systems = mongoose.model('System', System);
-  Appliances = mongoose.model('Appliance', Appliance)
+  Appliances = mongoose.model('Appliance', Appliance);
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
