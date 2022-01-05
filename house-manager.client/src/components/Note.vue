d74272eb66783ba9460da505c228564b22eb3141
ShereneC <spyhopper@cableone.net>
2022-01-05 12:48:11 -0700

    why creator.picture not showing up???

--- house-manager.client/src/components/Note.vue
+++ house-manager.client/src/components/Note.vue
@@ -7,6 +7,8 @@
   </div>
   <div class="col-3 d-flex">
     <h6>{{note.creator.name}}</h6>
+    <!-- Why is creator.picture not working but creator.name is???? -->
+    <h6>{{note.creator.picture}}</h6>
     <img :src="note.creator.picture" alt="profile image" class="profile-pic ml-2" />
   </div>
 
@@ -27,7 +29,7 @@
         noteCreatedAt: computed(() => {
           const d = new Date(props.note.createdAt)
           return new Intl.DateTimeFormat('en-US').format(d)
-        })
+        }),
       }
     },
     components: {}
