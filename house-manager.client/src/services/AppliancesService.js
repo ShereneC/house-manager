157331a30528ca5f4c250d643e702413b03f2d26
ShereneC <spyhopper@cableone.net>
2022-01-05 12:51:55 -0700

    appl service

--- house-manager.client/src/services/AppliancesService.js
+++ house-manager.client/src/services/AppliancesService.js
@@ -6,6 +6,16 @@ class AppliancesService {
     const res = await api.get('api/appliances')
     AppState.appliances = res.data
   }
+
+  async setActiveAppliance(id) {
+    const res = await api.get('api/appliances/' + id)
+    AppState.activeAppliance = res.data
+  }
+
+  async getNotesByApplianceId(id) {
+    const res = await api.get('api/appliances/' + id + '/notes')
+    AppState.notes[id] = res.data
+  }
 }
 
 export const appliancesService = new AppliancesService()
